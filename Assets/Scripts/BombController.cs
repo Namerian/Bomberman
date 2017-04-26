934df90ba8dd27df034c82cf9e78812151fa33dc
Unknown <patrickmonville@hotmail.com>
2017-04-26 16:25:54 +0200

    backup

---
+++ Assets/Scripts/BombController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BombController : NetworkBehaviour
+{
+    private static readonly Vector2[] DIRECTIONS = { new Vector2(0, 1), new Vector2(1, 0), new Vector2(0, -1), new Vector2(-1, 0) };
+
+    [SerializeField]
+    private float _timer;
+
+    [SerializeField]
+    private GameObject _explosionPrefab;
+
+    [SerializeField]
+    private float _tileDelay = 0.05f;
+
+    private bool _initialized = false;
+    private int _range;
+
+    public void Initialize(int range)
+    {
+        _initialized = true;
+        _range = range;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!_initialized)
+            Destroy(this.gameObject);
+        else
+            StartCoroutine(ExplodeCoroutine());
+    }
+
+    private IEnumerator ExplodeCoroutine()
+    {
+        yield return new WaitForSeconds(_timer);
+        Debug.Log("Bomb explodes");
+
+        Vector2 worldPos = this.transform.position;
+        Vector2 tilePos = new Vector2(Mathf.Round(worldPos.x), Mathf.Round(worldPos.y));
+
+        CmdSpawnExplosion(tilePos, 0);
+
+        Vector2 currentPos;
+        bool[] spread = { true, true, true, true };
+
+        for (int r = 1; r < _range; r++)
+        {
+            for (int d = 0; d < 4; d++)
+            {
+                if (!spread[d])
+                    continue;
+
+                currentPos = tilePos + r * DIRECTIONS[d];
+
+                RaycastHit2D[] hits = Physics2D.RaycastAll(currentPos, Vector2.zero);
+                bool noWall = true;
+
+                foreach (RaycastHit2D hit in hits)
+                {
+                    if (hit.collider.CompareTag("FixedWall"))
+                    {
+                        spread[d] = false;
+                        noWall = false;
+                        //break;
+                    }
+                    else if (hit.collider.CompareTag("DestructibleWall"))
+                    {
+                        //Destroy(hit.collider.gameObject);
+                        CmdSpawnExplosion(currentPos, _tileDelay * r);
+                        noWall = false;
+                        //break;
+                    }
+                }
+
+                if (noWall)
+                    CmdSpawnExplosion(currentPos, _tileDelay * r);
+            }
+        }
+
+        Destroy(this.gameObject);
+    }
+
+    [Command]
+    private void CmdSpawnExplosion(Vector2 pos, float delay)
+    {
+        GameObject explosion = Instantiate(_explosionPrefab, pos, _explosionPrefab.transform.rotation);
+        explosion.GetComponent<ExplosionScript>().Initialize(delay);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameController : NetworkBehaviour
+{
+    [SerializeField]
+    private int _mapSize;
+
+    [SerializeField]
+    private Transform _wallHolder;
+
+    [SerializeField]
+    private GameObject _destructibleWallPrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (this.isServer)
+        {
+            List<Vector2> destructibleWallPositions = GenerateDestructibleWallPositions();
+
+            foreach (Vector2 position in destructibleWallPositions)
+            {
+                CmdGenerateDestructibleWall(position);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+			Application.Quit ();
+#endif
+        }
+    }
+
+    private List<Vector2> GenerateDestructibleWallPositions()
+    {
+        List<Vector2> positions = new List<Vector2>();
+        int halfSize = _mapSize / 2;
+        System.Random random = new System.Random();
+
+        for (int x = -halfSize; x <= halfSize; x++)
+        {
+            for (int y = -halfSize; y <= halfSize; y++)
+            {
+                if (!CheckForWall(new Vector2(x, y))
+                    && (Mathf.Abs(x) != halfSize || Mathf.Abs(y) != halfSize)
+                    && (random.Next(1, 11) > 6)
+                    && !(Mathf.Abs(y) == halfSize && (Mathf.Abs(x) == halfSize - 1))
+                    && !(Mathf.Abs(x) == halfSize && (Mathf.Abs(y) == halfSize - 1)))
+                {
+                    positions.Add(new Vector2(x, y));
+                }
+            }
+        }
+
+        return positions;
+    }
+
+    [Command]
+    private void CmdGenerateDestructibleWall(Vector2 position)
+    {
+        GameObject wall = Instantiate(_destructibleWallPrefab, position, _destructibleWallPrefab.transform.rotation);
+        wall.transform.parent = _wallHolder;
+    }
+
+    private bool CheckForWall(Vector2 tilePos)
+    {
+        RaycastHit2D[] hits = Physics2D.RaycastAll(tilePos, Vector2.zero);
+
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider.CompareTag("FixedWall") || hit.collider.CompareTag("DestructibleWall"))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
